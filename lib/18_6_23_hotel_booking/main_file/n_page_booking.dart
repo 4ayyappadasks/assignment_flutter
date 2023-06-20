fbee79462f662b8aa36ed3edd6394954e755e713
4ayyappadasks <ayyappanks141@gmail.com>
2023-06-20 10:57:32 +0530

    commit

---
+++ lib/18_6_23_hotel_booking/main_file/n_page_booking.dart
@@ -0,0 +1,22 @@
+// import 'package:flutter/material.dart';
+//
+// class nmb extends StatefulWidget {
+//   const nmb({Key? key}) : super(key: key);
+//
+//   @override
+//   State<nmb> createState() => _nmbState();
+// }
+//
+// class _nmbState extends State<nmb> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//           child: Container(
+//         height: MediaQuery.of(context).size.height,
+//         width: MediaQuery.of(context).size.width,
+//             child: ListView(children: dum,)
+//       )),
+//     );
+//   }
+// }
--- lib/18_6_23_hotel_booking/main_file/page1_home.dart
+++ lib/18_6_23_hotel_booking/main_file/page1_home.dart
@@ -20,55 +20,6 @@ class p1 extends StatefulWidget {
 }
 
 class _p1State extends State<p1> {
-  var rate = [
-    "180/night",
-    "190/night",
-    "200/night",
-    "210/night",
-    "220/night",
-    "220/night",
-    "230/night",
-    "240/night",
-    "250/night",
-    "260/night",
-    "270/night",
-  ];
-  var names = [
-    "Crown plasa",
-    "king",
-    "queen",
-    "star",
-    "great",
-    "rose",
-    "lifa",
-    "kik spot",
-    "star kingdom",
-    "nation",
-  ];
-  var img =[
-    "assets/images/ho1.jpg",
-    "assets/images/ho2.jpg",
-    "assets/images/ho3.jpg",
-    "assets/images/ho4.jpg",
-    "assets/images/ho5.jpg",
-    "assets/images/ho6.jpg",
-    "assets/images/ho7.jpg",
-    "assets/images/ho8.jpg",
-    "assets/images/ho9.jpg",
-    "assets/images/ho10.jpg",
-  ];
-  var img2 =[
-    "assets/images/hoo1.jpg",
-    "assets/images/hoo2.jpg",
-    "assets/images/hoo3.jpg",
-    "assets/images/hoo4.jpg",
-    "assets/images/hoo5.jpg",
-    "assets/images/hoo6.jpg",
-    "assets/images/hoo7.jpg",
-    "assets/images/hoo8.jpg",
-    "assets/images/hoo9.jpg",
-    "assets/images/ho1.jpg",
-  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -89,10 +40,6 @@ class _p1State extends State<p1> {
                   rating: Text("${e["ratings"]}",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.blue)),
                   immg: AssetImage(e["img"]),)
               ).toList(),)
-            // ListView.builder(
-            //   scrollDirection: Axis.horizontal,
-            //   itemCount: 20,
-            //   itemBuilder: (context, index) => topsc(),),
           ),
           Text("HOTEL PACKAGE",style: TextStyle(color: Colors.black,fontSize: 20)),
           Container(
